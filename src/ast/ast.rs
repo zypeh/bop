0f90218f4aa48559c1107f8f844acb7c1f15d6c5
zypeh <zypeh.geek@gmail.com>
2019-05-13 16:32:48 +0800

    WIP EBNF parser, recursive nestsed choice bug

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -1,17 +1,18 @@
 
 /// A parse tree has nodes corresponding directly to the rules of a grammar.
-#[derive(Debug)]
-pub enum ParseTree {
+#[derive(Debug, Clone)]
+pub enum ParseTree <'a> {
     /// equivalent to ""
     Empty,
     /// terminal symbol
-    Terminal(String),
+    Terminal(&'a str),
     /// identifier, in this case it is non-terminal symbol
-    NonTerminal(String),
+    // NonTerminal(&'a str),
+    NonTerminalDefinition(&'a str, Box<ParseTree<'a>>),
     /// ordered choice
-    Choice(Vec<ParseTree>),
+    Choice(Vec<ParseTree<'a>>),
     /// optional choice
-    Optional(Box<ParseTree>),
+    Optional(Box<ParseTree<'a>>),
     /// repetition
-    Many(Box<ParseTree>),
+    Many(Box<ParseTree<'a>>),
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ extern crate pest;
 // use std::collections::HashMap;
 use std::fs;
 use pest::Parser;
+use pest::error::Error;
+
+mod ast;
+use ast::ast::ParseTree;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
@@ -11,10 +15,65 @@ pub struct EBNFParser;
 
 fn main() {
     let unparsed_file = fs::read_to_string("./src/test.bop").expect("cannot read file");
+    // let _file = EBNFParser::parse(Rule::bop, &unparsed_file)
+    //     .expect("unsuccessful parse") // unwrap the parse result
+    //     .next().unwrap(); // get and unwrap the `file` rule; never fails
 
-    let _file = EBNFParser::parse(Rule::bop, &unparsed_file)
-        .expect("unsuccessful parse") // unwrap the parse result
-        .next().unwrap(); // get and unwrap the `file` rule; never fails
+    // dbg!(_file);
+    parse(&unparsed_file).expect("unsuccessful parse");
+    println!("Successfully parsed !");
+}
 
-    dbg!(_file);
+fn parse(source: &str) -> Result<Vec<ParseTree>, Error<Rule>> {
+    let mut ast = vec![];
+
+    let pairs = EBNFParser::parse(Rule::bop, &source)?;
+
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::bop => {
+                let statements = pair.into_inner();
+                for statement in statements {
+                    ast.push(build_ast_from_production(statement));
+                }
+            },
+            Rule::EOI => {},
+            _ => unreachable!("parse()")
+        }
+    }
+
+    Ok(ast)
+}
+
+fn build_ast_from_production(statement: pest::iterators::Pair<Rule>) -> ParseTree {
+    let mut pairs = statement.into_inner();
+    let identifier = pairs.next().unwrap().as_str();
+    let expression = build_ast_from_expressions(pairs.next().unwrap().into_inner());
+
+    dbg!(ParseTree::NonTerminalDefinition(identifier, Box::new(expression.clone())));
+    ParseTree::NonTerminalDefinition(identifier, Box::new(expression))
+}
+
+// alternative = { string | group | option | repetition }
+fn build_ast_from_expressions(pairs: pest::iterators::Pairs<Rule>) -> ParseTree {
+    let mut alternatives = vec![];
+    for pair in pairs {
+
+        let ast = match pair.as_rule() {
+            Rule::string => {
+                match pair.into_inner().next().unwrap().as_str() {
+                    "" => ParseTree::Empty,
+                    x => ParseTree::Terminal(x),
+                }
+            },
+            Rule::expression => build_ast_from_expressions(pair.into_inner()),
+            Rule::alternative => build_ast_from_expressions(pair.into_inner()),
+            Rule::group => build_ast_from_expressions(pair.into_inner()),
+            Rule::option => ParseTree::Optional(Box::new(build_ast_from_expressions(pair.into_inner()))),
+            Rule::repetition => ParseTree::Many(Box::new(build_ast_from_expressions(pair.into_inner()))),
+            _ => unreachable!(pair),
+        };
+        alternatives.push(ast);
+    };
+    ParseTree::Choice(alternatives)
 }
